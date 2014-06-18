ae5ef2239f9e02142a2b1b678ec7f8a4d12f1b21
matias <mgonnet3@gmail.com>
2014-06-17 23:31:04 -0300

    En Almacen: agregado memberMedicamento, getInstance, destructor.

---
+++ source/interfacesYControladores/Almacen.cpp
@@ -0,0 +1,57 @@
+/*
+ * Almacen.cpp
+ *
+ *  Created on: 14/06/2014
+ *      Author: matias
+ */
+
+#include "../../headers/interfacesYControladores/Almacen.h"
+#include "../../headers/clases/CategoriaPS.h"
+#include "../../headers/clases/Usuario.h"
+
+#include <stdio.h>
+
+Almacen* Almacen::instancia=NULL;
+
+Almacen::Almacen() { }
+
+Almacen* Almacen::getInstance()
+{
+	if(instancia==NULL)
+		instancia=new Almacen;
+	return instancia;
+}
+
+set<Medicamento*> Almacen::getMedicamentos() { return medicamentos; }
+
+set<Usuario*> Almacen::getUsuarios() { return usuarios; }
+
+set<CategoriaPS*> Almacen::getCategoriasPS() { return categorias; }
+
+void Almacen::addMedicamento(Medicamento* med) { medicamentos.insert(med); }
+
+void Almacen::addUsuario(Usuario* usu) { usuarios.insert(usu); }
+
+void Almacen::addCategoriaPS(CategoriaPS* catPS) { categorias.insert(catPS); }
+
+bool Almacen::memberMedicamento(string nombre) { return true; }
+
+
+Almacen::~Almacen()
+{
+	//Primero debo vaciar todas las colecciones
+	set<Medicamento*>::iterator itMed;
+	for( itMed = medicamentos.begin(); itMed != medicamentos.end() ; ++itMed)
+		delete (*itMed);
+
+	set<Usuario*>::iterator itUser;
+	for ( itUser = usuarios.begin() ; itUser != usuarios.end() ; ++itUser )
+		delete (*itUser);
+
+	set<CategoriaPS*>::iterator itCat;
+	for ( itCat = categorias.begin() ; itCat != categorias.end() ; ++itCat )
+		delete (*itCat);
+
+	//Ahora seteo el Almacen como sin instancia
+	instancia = NULL;
+}
